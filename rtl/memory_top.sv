d4f3780ae23608ff385cf8a19ce7218b3b0001e6
Joel <nzmjoel@gmail.com>
2024-11-30 14:10:41 +0000

    adjusted input output for memory_top

--- rtl/memory_top.sv
+++ rtl/memory_top.sv
@@ -1,18 +1,18 @@
 module memory_top #(
     parameter DATA_WIDTH = 32     
 ) (
-    logic input clk
+    input logic                     clk,
+    input logic                     ResultSrc, // Connects to control unit
+    input logic                     MemWrite, // Connects to control unit
+    input logic [DATA_WIDTH-1:0]    ALUResult, //ALUResult should be 32 bit as the ALU output is 32 bit but i have my Address 
+    input logic [DATA_WIDTH-1:0]    WriteData, //Connects to reg file
+
+    output logic [DATA_WIDTH-1:0]   Result // Connects to WD3 of reg
 );
     
-logic [DATA_WIDTH-1:0] ALUResult; //ALUResult should be 32 bit as the ALU output is 32 bit but i have my Address 
-logic [DATA_WIDTH-1:0] WriteData; //Connects to reg file
-logic [DATA_WIDTH-1:0] ALUcompare; //This is the wire that muxes with ReadData
 logic [DATA_WIDTH-1:0] ReadData; // Connects to Mux
-logic [DATA_WIDTH-1:0] MemWrite; // Connects to control unit 
-logic [DATA_WIDTH-1:0] ResultSrc; // Connects to control unit
-logic [DATA_WIDTH-1:0] Result; // Connects to WD3 of reg
 
-datamem mydata (
+datamem data_mem_mod (
     .a(ALUResult),
     .wd(WriteData),
     .clk(clk),
@@ -21,7 +21,7 @@ datamem mydata (
 );
 
 mux mymux (
-    .in0(ALUcompare),
+    .in0(ALUResult),
     .in1(ReadData),
     .sel(ResultSrc),
     .out(Result)
