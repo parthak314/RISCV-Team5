52ee61be3c2ab71ee950584a4df475e74c4fe663
parthak314 <parthakfree@gmail.com>
2024-12-01 18:31:36 +0000

    datamem unused bits off

--- rtl/datamem.sv
+++ rtl/datamem.sv
@@ -1,15 +1,16 @@
+/* verilator lint_off UNUSED */
 module datamem #(
     parameter DATA_WIDTH = 32 
 
 ) (
-    input logic [DATA_WIDTH-1:0]    a, //Input address for data 
+    input logic [DATA_WIDTH-1:0]    a, // Input address for data 
     input logic [DATA_WIDTH-1:0]    wd, // Data to be written in
     input logic                     clk, // Allows for it to be clocked
-    input logic                     we, // Wrte enable
+    input logic                     we, // Write enable
     output logic [DATA_WIDTH-1:0]   rd //Read Data 
 );
 
-logic [DATA_WIDTH-1:0] ram_array [32'h0001FFFF:0]; //I'm not using 2**ADDRESS_WIDTH as this piece connects to the ALU and we can just have it be 32 bit
+logic [DATA_WIDTH-1:0] ram_array [32'h0001FFFF:0]; // I'm not using 2**ADDRESS_WIDTH as this piece connects to the ALU and we can just have it be 32 bit
 
 initial begin
     $display("Loading ram");
@@ -21,7 +22,7 @@ always_comb begin
 end
 
 always @(posedge clk) begin
-    if (we == 1'b1) //Checks if we can write 
+    if (we) //Checks if we can write 
         ram_array[a] <= wd; //Assigns value of read data to be assigned at address a
 end
 
