d0cc8aa840fcbaa0ad100dffa955e7bfdc9b6588
parthak314 <parthakfree@gmail.com>
2024-12-01 01:32:55 +0000

    created top module

--- tb/bash/control_test.sh
+++ tb/bash/control_test.sh
@@ -2,13 +2,13 @@
 
 # Translate Verilog -> C++ including testbench
 verilator -Wall --trace \
-          -cc signextend.sv \
-          --exe signextend_test_tb.cpp \
-          -o Vsignextend \
+          -cc ../../rtl/control.sv \
+          --exe ../tests/control_test_tb.cpp \
+          -o Vcontrol \
           -LDFLAGS "-lgtest -lgtest_main -lpthread"
 
 # Build C++ project with automatically generated Makefile
-make -j -C obj_dir/ -f Vsignextend.mk
+make -j -C obj_dir/ -f Vcontrol.mk
 
 # Run executable simulation file
-./obj_dir/Vsignextend
+./obj_dir/Vcontrol
