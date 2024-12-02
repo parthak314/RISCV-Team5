c581468579dc9ce6202c8365b63bb6e5edd69588
parthak314 <parthakfree@gmail.com>
2024-12-02 23:44:35 +0000

    file reorganisation

--- tb/build_execute.sh
+++
@@ -1,7 +0,0 @@
-rm -rf obj_dir
-verilator   -Wall --cc --trace ../rtl/execute_top.sv \
-            -y ../rtl/ \
-            --exe ./our_tests/sim_execute.cpp \
-            
-make -j -C obj_dir/ -f Vexecute_top.mk Vexecute_top
-obj_dir/Vexecute_top
