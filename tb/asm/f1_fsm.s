cf771bd4e32303ca6c11952d5d53ed94e64ba9ff
parthak314 <parthakfree@gmail.com>
2024-12-13 13:32:02 +0000

    f1 assembly comments added

--- tb/asm/f1_fsm.s
+++ tb/asm/f1_fsm.s
@@ -2,14 +2,12 @@
 .globl main
 
 main:
-    # Initialize state and output registers
     li      a0, 0x0                # a0 holds the current state (S_0 initially)
     li      a2, 0                  # a2 is used for cmd_delay
     li      a3, 0                  # a3 is used for cmd_seq
     addi    t1, zero, 0b11111111   # t1 = 11111111 (Value at State 8)
 
 shift_loop:
-    # State machine logic with shifting
     slli    a0, a0, 1              # Shift left
     ori     a0, a0, 0b1            # Replace the gap with a 1 (increment states)
     bne     a0, t1, state_output   # If not at State 8, jump to output logic
